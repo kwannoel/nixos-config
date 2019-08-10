916befc26e870deeb2420533cd18fe86f07a3a19
Noel Kwan <noelkwan1998@gmail.com>
2019-08-10 23:25:54 +0800

    Isolate device specific config

--- configuration.nix
+++ configuration.nix
@@ -15,17 +15,8 @@
       ./psql.nix
       ./fonts.nix
       ./emacs.nix
+      ./boot.nix
     ];
-
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-  boot.supportedFilesystems = [ "ntfs" ];
 
   # Allow firefox and other unfree pkgs
   nixpkgs.config.allowUnfree = true;
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Set up
 
+symlink device specific configuration:
+> ln -s <device_folder>/<config_file> <config_file>
+
+E.g.
+
+> ln -s thinkpad/boot.nix boot.nix
+> ln -s thinkpad/hardware-configuration.nix hardware-configuration.nix
+
 Run init.sh to setup the nix system
