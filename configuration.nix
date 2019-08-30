9479be4e806490622acf054a1650419492150ee8
Noel Kwan <noelkwan1998@gmail.com>
2019-08-30 10:52:03 +0800

    Add bluetooth

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,9 @@
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
