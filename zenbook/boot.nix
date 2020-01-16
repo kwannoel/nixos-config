3a74a64bd1d86bddb17864295900b5beb4d8b845
Noel Kwan <noelkwan1998@gmail.com>
2020-01-16 17:12:50 +0800

    Tweak video driver

--- zenbook/boot.nix
+++ zenbook/boot.nix
@@ -3,7 +3,8 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  services.xserver.videoDrivers = ["modesetting"];
+  services.xserver.videoDrivers = ["nvidia_modeset"];
+  # services.xserver.videoDrivers = ["modesetting"]; if the above throws any issues
   boot.blacklistedKernelModules = ["nouveau"];
   i18n.consoleFont = "latarcyrheb-sun32";
 }
