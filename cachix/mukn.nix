683b62d08f08272c0e01a0aa84a9e4577f5ae494
Noel Kwan <noelkwan1998@gmail.com>
2022-01-23 14:26:55 +0800

    Add iohk, mukn cachix

---
+++ cachix/mukn.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://mukn.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "mukn.cachix.org-1:ujoZLZMpGNQMeZbLBxmOcO7aj+7E5XSnZxwFpuhhsqs="
+    ];
+  };
+}
+    
